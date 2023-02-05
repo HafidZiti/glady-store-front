b77652f25b7c06d1729b7c9b252b112ff6dc4ec7
Hafid <ziti.hafid@gmail.com>
2023-02-05 17:59:12 +0100

    :recycle:rework CalculatorComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,27 @@
+import { CalculatorComponentValue } from './model/calculatorComponentValue';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { CalculatorComponent } from './components/calculator/calculator.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CalculatorComponent],
+  imports: [CalculatorComponent, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'glady-store-front';
+
+  private readonly initialValue: CalculatorComponentValue = {
+    value: 42,
+    cards: [20, 22],
+  };
+
+  protected readonly calculatorForm = new FormGroup({
+    calculator: new FormControl<CalculatorComponentValue>(this.initialValue),
+  });
+
   constructor() {}
 
   protected handleCalculatorEvent(amount: number) {
--- src/app/components/calculator/calculator.component.ts
+++ src/app/components/calculator/calculator.component.ts
@@ -1,62 +1,83 @@
+import { CalculatorComponentValue } from './../../model/calculatorComponentValue';
 import {
   ChangeDetectionStrategy,
   Component,
   ChangeDetectorRef,
   Output,
   EventEmitter,
+  forwardRef,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import {
-  ReactiveFormsModule,
-  FormGroup,
-  FormControl,
-  Validators,
+  NG_VALUE_ACCESSOR,
+  ControlValueAccessor,
+  FormsModule,
 } from '@angular/forms';
+import { take } from 'rxjs';
 import { CalculatorService } from '../../calculator.service';
 import { Nullable } from '../../helpers/types';
+import { CardFormatPipe } from '../../pipes/card-format.pipe';
 
 @Component({
   selector: 'app-calculator',
   standalone: true,
-  imports: [HttpClientModule, ReactiveFormsModule, CommonModule],
-  providers: [CalculatorService],
+  imports: [HttpClientModule, FormsModule, CommonModule, CardFormatPipe],
+  providers: [
+    CalculatorService,
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: forwardRef(() => CalculatorComponent),
+    },
+  ],
   templateUrl: './calculator.component.html',
   styleUrls: ['./calculator.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CalculatorComponent {
+export class CalculatorComponent implements ControlValueAccessor {
   @Output()
   enteredAmount = new EventEmitter<number>();
 
-  protected readonly shopForm = new FormGroup({
-    amount: new FormControl<number | null>(null, [
-      Validators.required,
-      Validators.min(0),
-    ]),
-  });
+  private amountInput: Nullable<number> = null;
 
-  protected cards: Array<number> = [];
   protected higherSuggestion: Nullable<number> = null;
   protected lowerSuggestion: Nullable<number> = null;
+  protected cards: Array<number> = [];
   protected isLoading = false;
-
-  get amount() {
-    return this.shopForm.get('amount')?.value;
-  }
+  protected isDisabled = false;
+  protected isValid = false;
+  protected isModified = false;
 
   constructor(
     private readonly calculatorService: CalculatorService,
     private readonly cd: ChangeDetectorRef
-  ) {
-    this.shopForm.get('amount')?.valueChanges.subscribe((x) => console.log(x));
+  ) {}
+
+  public writeValue(calculatorComponentValue: CalculatorComponentValue): void {
+    const { value, cards } = calculatorComponentValue;
+    this.amount = value;
+    this.cards = cards;
   }
 
-  protected onSubmit(): void {
-    if (this.shopForm.invalid || !this.amount) return;
+  public registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  public registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  public setDisabledState?(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
+  }
+
+  protected onClick(): void {
+    if (!this.amount) return;
     this.isLoading = true;
     this.calculatorService
       .searchCards(this.amount)
+      .pipe(take(1))
       .subscribe((combinations) => {
         this.isLoading = false;
         if (combinations.equal) {
@@ -71,21 +92,41 @@ export class CalculatorComponent {
     this.enteredAmount.emit(this.amount);
   }
 
+  protected handleValueChanges($event: number): void {
+    this.isModified = true;
+    this.isValid = $event > 0;
+  }
+
   protected increase() {
-    this.shopForm.get('amount')?.patchValue(this.higherSuggestion);
+    this.isModified = true;
+    this.amount = this.higherSuggestion;
   }
 
   protected decrease() {
-    this.shopForm.get('amount')?.patchValue(this.lowerSuggestion);
+    this.isModified = true;
+    this.amount = this.lowerSuggestion;
   }
 
   protected trackByFn(index: number): number {
     return index;
   }
 
+  private onChange(value: CalculatorComponentValue): void {}
+
+  private onTouched(): void {}
+
   private reset(): void {
     this.cards = [];
     this.lowerSuggestion = null;
     this.higherSuggestion = null;
+    this.isModified = false;
+  }
+
+  get amount() {
+    return this.amountInput;
+  }
+
+  set amount(value: Nullable<number>) {
+    this.amountInput = value;
   }
 }
